68100a6f33661a97b41e8fb23b1fe5a1cdd02ea0
j8seangel <jseangel@gmail.com>
2020-08-03 11:09:16 +0200

    use @globalfishingwatch npm scope for publishing

---
+++ types/index.d.ts
@@ -0,0 +1,26 @@
+declare module '@globalfishingwatch/mapbox-gl' {
+  import * as MapboxGl from 'mapbox-gl'
+  export = MapboxGl
+}
+declare module '@globalfishingwatch/react-map-gl' {
+  import * as ReactMapGl from 'react-map-gl'
+  export = ReactMapGl
+}
+declare module '@globalfishingwatch/react-map-gl/dist/esm/components/marker' {
+  import { Marker } from '@globalfishingwatch/react-map-gl'
+  export = Marker
+}
+declare module '@globalfishingwatch/react-map-gl/dist/esm/components/popup' {
+
+  import { Popup } from '@globalfishingwatch/react-map-gl'
+  export = Popup
+}
+declare module '@globalfishingwatch/react-map-gl/dist/esm/components/scale-control' {
+
+  import { ScaleControl } from '@globalfishingwatch/react-map-gl'
+  export = ScaleControl
+}
+declare module '@globalfishingwatch/react-map-gl/dist/esm/utils/transition/viewport-fly-to-interpolator' {
+  import { FlyToInterpolator } from '@globalfishingwatch/react-map-gl'
+  export = FlyToInterpolator
+}

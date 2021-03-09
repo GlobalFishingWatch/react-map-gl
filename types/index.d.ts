cc9af4ae6788db3a68a18ae703c9d4f310ff9ce1
j8seangel <jseangel@gmail.com>
2021-03-09 17:56:37 +0100

    bump fourwings aggregate version

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,20 +2,27 @@ declare module '@globalfishingwatch/react-map-gl' {
   import * as ReactMapGl from 'react-map-gl'
   export = ReactMapGl
 }
+
+declare module '@globalfishingwatch/react-map-gl/dist/esm/components/use-map-context' {
+  import { _MapContext } from '@globalfishingwatch/react-map-gl'
+  export = _MapContext
+}
+
 declare module '@globalfishingwatch/react-map-gl/dist/esm/components/marker' {
   import { Marker } from '@globalfishingwatch/react-map-gl'
   export = Marker
 }
-declare module '@globalfishingwatch/react-map-gl/dist/esm/components/popup' {
 
+declare module '@globalfishingwatch/react-map-gl/dist/esm/components/popup' {
   import { Popup } from '@globalfishingwatch/react-map-gl'
   export = Popup
 }
-declare module '@globalfishingwatch/react-map-gl/dist/esm/components/scale-control' {
 
+declare module '@globalfishingwatch/react-map-gl/dist/esm/components/scale-control' {
   import { ScaleControl } from '@globalfishingwatch/react-map-gl'
   export = ScaleControl
 }
+
 declare module '@globalfishingwatch/react-map-gl/dist/esm/utils/transition/viewport-fly-to-interpolator' {
   import { FlyToInterpolator } from '@globalfishingwatch/react-map-gl'
   export = FlyToInterpolator
